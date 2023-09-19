8dd08ed6a7f60cd9edfeae196a25f19f228db9ce
haoshuxin <1724486572@qq.com>
2023-09-19 16:52:35 +0800

    基本框架完善02

--- app/src/main/java/com/example/secondhandplatforms/MyAdapter.java
+++ app/src/main/java/com/example/secondhandplatforms/MyAdapter.java
@@ -3,18 +3,30 @@ package com.example.secondhandplatforms;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private List<MyDataItem> dataItemList;
+    private OnItemClickListener onItemClickListener;
 
     public MyAdapter(List<MyDataItem> dataItemList) {
         this.dataItemList = dataItemList;
+    }
+
+    // 定义一个接口用于处理点击事件
+    public interface OnItemClickListener {
+        void onItemClick(MyDataItem dataItem);
+        void onPublishClick(MyDataItem dataItem);
+    }
+
+    // 设置点击事件监听器
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
     }
 
     @NonNull
@@ -32,6 +44,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         holder.textTitle.setText(dataItem.getContent());
         holder.textPrice.setText("¥" + dataItem.getPrice());
         holder.textAddress.setText(dataItem.getAddr());
+
+        // 设置项目的点击事件
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(dataItem);
+                }
+            }
+        });
+
+        // 设置发布按钮的点击事件
+        holder.buttonPublish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onPublishClick(dataItem);
+                }
+            }
+        });
     }
 
     @Override
@@ -43,12 +75,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public TextView textTitle;
         public TextView textPrice;
         public TextView textAddress;
+        public Button buttonPublish; // 发布按钮
 
         public ViewHolder(View view) {
             super(view);
             textTitle = view.findViewById(R.id.textTitle);
             textPrice = view.findViewById(R.id.textPrice);
             textAddress = view.findViewById(R.id.textAddress);
+            buttonPublish = view.findViewById(R.id.buttonPublish); // 发布按钮
         }
     }
 }
--- app/src/main/java/com/example/secondhandplatforms/SavedFragment.java
+++ app/src/main/java/com/example/secondhandplatforms/SavedFragment.java
@@ -1,63 +1,240 @@
 package com.example.secondhandplatforms;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SavedFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class SavedFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String API_ENDPOINT = "http://47.107.52.7:88/member/tran/goods/save";
+    private static final String APP_ID = "8d7539b50797443788485b81f0660ce1";
+    private static final String APP_SECRET = "3636148e54bbef9044a5a8647598c1ee004a4";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private List<MyDataItem> dataItemList;
 
     public SavedFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SavedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SavedFragment newInstance(String param1, String param2) {
-        SavedFragment fragment = new SavedFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_publish, container, false);
+        // 初始化 RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        dataItemList = new ArrayList<>();
+        adapter = new MyAdapter(dataItemList);
+
+        // 添加发布按钮的点击事件监听器
+        adapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(MyDataItem dataItem) {
+                // 处理点击商品卡片事件
+                Log.d("id", "执行了");
+            }
+
+            @Override
+            public void onPublishClick(MyDataItem dataItem) {
+                // 处理点击发布按钮事件，使用 dataItem 对象的数据，发送请求
+                sendPublishRequest(dataItem);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
+        // 发送网络请求获取数据
+        fetchData();
+
+        return view;
     }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+    private void fetchData() {
+        OkHttpClient client = new OkHttpClient();
+
+        // 创建请求并添加请求头
+        Request request = new Request.Builder()
+                .url(API_ENDPOINT + "?current=1&userId=" + MainActivity.UserId)
+                .addHeader("appId", APP_ID)
+                .addHeader("appSecret", APP_SECRET)
+                .build();
+
+        // 异步执行请求
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // 网络请求失败
+                e.printStackTrace();
+                // 在此处理失败情况，例如显示错误消息
+                displayErrorMessage("网络请求失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    // 网络请求成功
+                    final String responseData = response.body().string();
+                    handleResponseData(responseData);
+                } else {
+                    // 网络请求失败
+                    // 在此处理失败情况，例如显示错误消息
+                    displayErrorMessage("网络请求失败");
+                }
+            }
+        });
+    }
+
+    private void handleResponseData(final String responseData) {
+        try {
+            JSONObject jsonObject = new JSONObject(responseData);
+            int code = jsonObject.getInt("code");
+            if (code == 200) {
+                JSONArray records = jsonObject.getJSONObject("data").getJSONArray("records");
+                Log.d("sb", records.toString());
+                for (int i = 0; i < records.length(); i++) {
+                    JSONObject record = records.getJSONObject(i);
+                    //获取对象进行渲染
+                    String id = record.getString("id");
+                    String content = record.getString("content");
+                    String price = record.getString("price");
+                    String addr = record.getString("addr");
+                    String imageCode = record.getString("imageCode");
+                    String typeid = record.getString("typeId");
+                    String typeName = record.getString("typeName");
+                    String userid = record.getString("tuserId");
+                    //解析商品出对象，并在此同时渲染
+                    MyDataItem dataItem = new MyDataItem(id, content, price, addr, imageCode, typeid, typeName, userid);
+                    dataItemList.add(dataItem);
+                }
+
+                // 更新 UI 线程中的数据
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+            } else {
+                String errorMsg = jsonObject.getString("msg");
+                displayErrorMessage("Error: " + errorMsg);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            displayErrorMessage("数据解析失败");
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved, container, false);
+    private void sendPublishRequest(MyDataItem dataItem) {
+        OkHttpClient client = new OkHttpClient();
+        // 创建发布请求并添加请求参数，例如使用 POST 请求
+        RequestBody requestBody = new FormBody.Builder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://47.107.52.7:88/member/tran/goods/change?id="+dataItem.getId()+"&content="+dataItem.getContent()
+                +"&addr="+dataItem.getAddr()+"&imageCode="+dataItem.getImageCode()+"&price="+dataItem.getPrice()+"&" +
+                        "typeId="+dataItem.getTypeid()+"&typeName="+dataItem.getTypeName()+"&userId="+dataItem.getUserid())
+                .addHeader("appId", APP_ID)
+                .addHeader("appSecret", APP_SECRET)
+                .post(requestBody)
+                .build();
+        // 异步执行发布请求
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // 发布请求失败
+                e.printStackTrace();
+                // 在此处理失败情况，例如显示错误消息
+                displayErrorMessage("发布请求失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    // 发布请求成功
+                    final String responseData = response.body().string();
+                    handlePublishResponse(responseData);
+                } else {
+                    // 发布请求失败
+                    // 在此处理失败情况，例如显示错误消息
+                    displayErrorMessage("发布请求失败");
+                }
+            }
+        });
+    }
+
+    // 处理发布响应
+    private void handlePublishResponse(String responseData) {
+        try {
+            JSONObject jsonObject = new JSONObject(responseData);
+            int code = jsonObject.getInt("code");
+            if (code == 200) {
+                displaySuccessMessage("发布成功");
+            } else {
+                String errorMsg = jsonObject.getString("msg");
+                displayErrorMessage("发布失败: " + errorMsg);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            displayErrorMessage("发布响应解析失败");
+        }
+    }
+
+    // 显示成功消息
+    private void displaySuccessMessage(final String message) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run(){
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+
+    private void displayErrorMessage(final String message) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
+
+
+
+
+
